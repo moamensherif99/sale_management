763e863cf1bcd1bce171534feb1948f430cdb4c1
Moamen Sherif Abdelkader <moamensherif19@gmail.com>
2025-01-27 20:31:46 +0200

    New Commit

--- models/sale_order.py
+++ models/sale_order.py
@@ -20,16 +20,17 @@ class SaleOrder(models.Model):
 
     exceed_discount_limit = fields.Boolean()
 
-
     def action_confirm(self):
-        res = super(SaleOrder, self).action_confirm()
-        manger_group = self.env.ref('sales_team.group_sale_manager', raise_if_not_found=False)
         for rec in self:
-            if manger_group and manger_group in self.env.user.groups_id:
-                if rec.state == 'waiting':
-                    rec.state = 'sale'
+            if self.env.user.has_group('sales_team.group_sale_manager') and rec.state == 'waiting':
+                rec.state = 'sale'
                 continue
-            if rec.exceed_discount_limit:
+            if rec.exceed_discount_limit :
                 rec.state = 'waiting'
-        return res
+                continue
+        confirmable_orders = self.filtered(lambda order: order.state in ['draft', 'sent'])
+        if confirmable_orders:
+            res = super(SaleOrder, confirmable_orders).action_confirm()
+            return res
+        return True
 
